f845e91f4546e39bebdd4d1cd4fc8108397e5fc5
unknown <akhilahima22@gmail.com>
2026-02-07 13:45:49 +0530

    first

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,34 +1,43 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../schema/userSchema')
+const User = require('../schema/userSchema');
 require('dotenv').config();
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
+
+const callbackURL = 'https://leaforaa.store/auth/google/callback';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: callbackURL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
         let user = await User.findOne({ googleId: profile.id });
         if (user) return done(null, user);
 
         user = new User({
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            googleId: profile.id
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
         });
+
         await user.save();
         return done(null, user);
-    } catch (err) {
+      } catch (err) {
         return done(err, null);
+      }
     }
-}));
+  )
+);
 
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => done(null, user))
-        .catch(err => done(err, null));
+  User.findById(id)
+    .then((user) => done(null, user))
+    .catch((err) => done(err, null));
 });
 
 module.exports = passport;
